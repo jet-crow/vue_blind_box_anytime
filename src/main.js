b5dc862d90110053435018a8ea80cd507a15abf5
Jet <1298874688@qq.com>
2023-04-07 15:51:05 +0800

    add img instructions

--- src/main.js
+++ src/main.js
@@ -1,9 +1,23 @@
-import {createApp} from 'vue'
+import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import './assets/css/reset.css'
 
+
 const app = createApp(App);
+
+/* 全局方法 */
+//获取本地图片
+/* 
+使用：
+ css请使用行内样式
+ :style="{backgroundImage:`url(${$getImgUrl('loginBg.jpg')})`}"
+ js：请调用
+    $getImgUrl('xxx.jpg')
+*/
+app.config.globalProperties.$getImgUrl = (src) => {
+    return "./images/" + src
+}
 
 app.use(router);
 app.mount('#app');
