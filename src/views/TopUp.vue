9f8552ede26d8bcf0926fff3d0ed1b5e8e86193b
Drunkcat7 <Miaopasc@gmail.com>
2023-04-07 17:26:33 +0800

    add topUp page finish

--- src/views/TopUp.vue
+++ src/views/TopUp.vue
@@ -1,6 +1,25 @@
 <template>
     <div class="bg">
-        
+        <main>
+            <aside>
+                <van-icon name="arrow-left" />
+                <van-icon name="question" />
+            </aside>
+            <section class="balance_box">
+                <span>余额</span>
+                <span class="balance">100</span>
+            </section>
+            <section class="top_up_button_box">
+                <ul>
+                    <li>20元</li>
+                    <li>30元</li>
+                    <li>50元</li>
+                    <li>100元</li>
+                    <li>300元</li>
+                </ul>
+            </section>
+            <div class="submit">立即支付</div>
+        </main>
     </div>
 </template>
 <script setup></script>
