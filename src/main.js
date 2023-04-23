6535e84adf8c26a414962cc73665321f26887059
Drunkcat7 <Miaopasc@gmail.com>
2023-04-23 13:33:24 +0800

    update 登录页 finish

--- src/main.js
+++ src/main.js
@@ -1,10 +1,15 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import './assets/css/reset.css'
-
+import {createApp} from 'vue';
+import App from './App.vue';
+import router from './router';
+import axios from 'axios';
+import qs from 'qs';
+import './assets/css/reset.css';
+import 'vant/es/toast/style';
+import {showSuccessToast, showFailToast} from 'vant';
 
 const app = createApp(App);
+axios.defaults.baseURL = '/api';
+// axios.defaults.baseURL = 'http://localhost:9090';
 
 /* 全局方法 */
 //获取本地图片
@@ -15,9 +20,37 @@ const app = createApp(App);
  js：请调用
     $getImgUrl('xxx.jpg')
 */
+
+
+/* 获取cookie的token放到headers中 */
+axios.interceptors.request.use(
+    function (config) {
+        let token = localStorage.getItem("token");
+        if (token) {
+            // 添加headers
+            config.headers['token'] = token; // token
+        }
+        return config;
+    },
+    function (error) {
+        // Do something with request error
+        return Promise.reject(error);
+    });
+
+app.config.globalProperties.$api = axios;
+app.config.globalProperties.$qs = qs;
+app.config.globalProperties.$showSuccessToast = showSuccessToast;
+app.config.globalProperties.$showFailToast = showFailToast;
+
+/* 全局方法 */
+//获取本地图片
 app.config.globalProperties.$getImgUrl = (src) => {
-    return "./images/" + src
-}
+    if (src === undefined) {
+        return "/";
+    }
+    // return require("./images/" + src)
+    return "./images/" + src;
+};
 
 app.use(router);
 app.mount('#app');
--- src/views/LoginAndRegistered.vue
+++ src/views/LoginAndRegistered.vue
@@ -4,20 +4,20 @@
             <h1>随时盲盒</h1>
 
             <label class="form_item no_border">
-                <input v-model="user" placeholder="usernames" />
-                <!-- <span>&nbsp</span> -->
-                <span>账号有误</span>
+                <input v-model="user" placeholder="usernames"/>
+                <span v-show="checkInput.user">&nbsp</span>
+                <span v-show="!checkInput.user">账号有误</span>
             </label>
             <label class="form_item no_border">
                 <input v-model="password" placeholder="password" />
-                <!-- <span>&nbsp</span> -->
-                <span>密码有误</span>
+                <span v-show="checkInput.password">&nbsp</span>
+                <span v-show="!checkInput.password">密码有误</span>
             </label>
             <!-- 注册part -->
             <label class="form_item no_border" v-show="!isLogin">
                 <input v-model="againPassword" placeholder="again password" />
-                <!-- <span>&nbsp</span> -->
-                <span>密码不一致</span>
+                <span v-show="checkInput.againPassword">&nbsp</span>
+                <span v-show="!checkInput.againPassword">密码不一致</span>
             </label>
 
             <div class="form_box_button">
@@ -34,17 +34,23 @@
     </main>
 </template>
 <script setup>
+// 引入js
+import {showSuccessToast, showFailToast} from 'vant';
+import {ref, reactive, getCurrentInstance} from 'vue';
+import router from "@/router";
 
-import { ref } from 'vue'
-import { useRouter } from 'vue-router'
+const {proxy} = getCurrentInstance();
 
 //data
 const user = ref();
 const password = ref();
 const againPassword = ref();
 const isLogin = ref(true);//是否为登录页面
-//路由
-const route = useRouter()
+const checkInput = reactive({
+    user: true,
+    password: true,
+    againPassword: true
+});
 
 function switchForm() {
     isLogin.value = !isLogin.value;
@@ -52,9 +58,51 @@ function switchForm() {
 
 //跳转
 function jump() {
-    if (isLogin) {
-        route.push("/index")
+    if (isLogin.value) {
+        //登录
+        proxy.$api.post('/account/login', proxy.$qs.stringify({
+            'user': user.value,
+            'password': password.value
+        })).then(res => {
+            // console.log(res.data);
+            if (res.data.status === 500) {
+                showFailToast('登錄失敗');
+                inputCheck(false);
+                return;
+            }
+            showSuccessToast('登錄成功');
+            // 前面的为设置cookies
+            localStorage.setItem("token", res.data.data.token);
+            localStorage.setItem("name", res.data.data.user);
+            localStorage.setItem("uid", res.data.data.uid);
+            //跳转主页
+            router.push('/index');
+        });
+    } else {//注册
+        //如果密码不一致直接返回
+        if (password.value !== againPassword.value) {
+            checkInput.againPassword = false;
+            return false;
+        }
+        proxy.$api.post("/account/reg", proxy.$qs.stringify({
+            'user': user.value,
+            'password': againPassword.value
+        })).then(r => {
+            inputCheck(true);
+            showSuccessToast("注册成功");
+            isLogin.value = true;
+        }).catch(_ => {
+            inputCheck(false);
+            showFailToast('注册失败');
+        });
     }
+}
+
+//error
+function inputCheck(b) {
+    checkInput.user = b;
+    checkInput.password = b;
+    checkInput.againPassword = b;
 }
 </script>
 <style scoped src="@/assets/css/form.css"></style>
